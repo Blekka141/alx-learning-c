a84e85b7efea81829a67fc77f06444d48b21ea36
Keegan <keeganjoubert@protonmail.com>
2024-02-01 10:05:00 -0800

    Hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,29 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves a value associated with a key.
- * @ht: The hash table.
- * @key: The key string.
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: table to retrieve value from
+ * @key: key to find value
  *
- * Return: The value associated with the element, or NULL if key couldn’t be found.
+ * Return: value associated with key, or NULL if key cannot be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *temp;
+	unsigned long int index;
+	hash_node_t *tmp;
 
-    if (ht == NULL || key == NULL)
-        return (NULL);
-
-    index = key_index((const unsigned char *)key, ht->size);
-
-    temp = ht->array[index];
-    while (temp != NULL)
-    {
-        if (strcmp(temp->key, key) == 0)
-            return (temp->value);
-        temp = temp->next;
-    }
-
-    return (NULL);
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
